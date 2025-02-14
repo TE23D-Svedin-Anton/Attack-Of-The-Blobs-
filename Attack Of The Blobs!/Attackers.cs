ba34e4a7e3c518784231264fc97edf8ffb32c194
anton svedin <anton.svedin@elev.ga.ntig.se>
2025-02-14 10:10:02 +0100

    Skrivit om min kod så den är lättare att läsa. fixat en ny triangel anfallare. fixat motståndarens anffalare. Fixat outlines på anfallarna. Fixat så anfallarna stannar när dom kommer fram till andra sidan

--- Attack Of The Blobs!/Attackers.cs
+++ Attack Of The Blobs!/Attackers.cs
@@ -1,17 +1,27 @@
     using System;
+using System.Drawing;
 using System.Numerics;
 using Raylib_cs;
 
     namespace Attackers;
-    public class RectAttacker
-    {
-        public Rectangle Rect;
-        public int Speed = +2;
-    }
 
     public class CircAttacker
-{
-    public Vector2 Center;
+    {
+    public Vector2 Position;
     public int radius = 40;
-    public int Speed = +2;
-}
+    public float Speed = +2.0f;
+    }
+
+    public class TriAttacker
+    {
+        public Vector2 Position;
+        public int Size = 80;
+
+        public float Speed = +0.8f;
+    }
+
+     public class RectAttacker
+    {
+        public Raylib_cs.Rectangle Rect;
+        public float Speed = +0.6f;
+    }
--- Attack Of The Blobs!/Program.cs
+++ Attack Of The Blobs!/Program.cs
@@ -1,70 +1,195 @@
 ﻿using System;
 using Raylib_cs;
 using Attackers;
-using TriAttacker;
 using System.Numerics;
 
-TriStats tri = new TriStats();
+float CooldownTime = 2.0f;
+float PlrLastTimeSpawned = 0.0f;
+float EnemyLastTimeSpawned = 0.0f;
 
-float CooldownTime = 1.0f;
-float LastTimeSpawned = 0.0f;
-
-List<RectAttacker> rectangles = [];
-List<CircAttacker> circles = [];
+List<RectAttacker> PlrRectangles = [];
+List<TriAttacker> PlrTriangles = [];
+List<CircAttacker> PlrCircles = [];
+List<RectAttacker> EnemyRectangles = [];
+List<TriAttacker> EnemyTriangles = [];
+List<CircAttacker> EnemyCircles = [];
 
 void SpawnButton() //Skapar en anfallare beroende på vilken knapp som trycks
 {
-    int RectWidth = 80;
-    int RectspawnX = 200 - (RectWidth/2);
-    int RectspawnY = 760 - (RectWidth/2);
-    Vector2 CircSpawn = new Vector2(200, 760);
-
-
-    if(Raylib.IsKeyPressed(KeyboardKey.Three) && ((float)Raylib.GetTime() - LastTimeSpawned >= CooldownTime))
+    int PlrSpawnX = 200;
+    int EnemySpawnX = 1300;
+    int SpawnY = 760;
+    Vector2 PlrCircSpawn = new Vector2(200, 760);
+    Vector2 EnemyCircSpawn = new Vector2(1300, 760);
+    
+    if(Raylib.IsKeyPressed(KeyboardKey.One) && ((float)Raylib.GetTime() - PlrLastTimeSpawned >= CooldownTime))
     {
-        rectangles.Add(new RectAttacker() {Rect = new Rectangle(RectspawnX, RectspawnY, 80, 80)}); //Skapar en ny Rektangel i rectangles listan
-        LastTimeSpawned = (float)Raylib.GetTime();   
+        PlrCircles.Add(new CircAttacker() {Position = PlrCircSpawn}); //Skapar en ny Rektangel i rectangles listan
+        PlrLastTimeSpawned = (float)Raylib.GetTime();   
     }
 
-    if(Raylib.IsKeyPressed(KeyboardKey.One) && ((float)Raylib.GetTime() - LastTimeSpawned >= CooldownTime))
+    if(Raylib.IsKeyPressed(KeyboardKey.Eight) && ((float)Raylib.GetTime() - EnemyLastTimeSpawned >= CooldownTime))
     {
-        circles.Add(new CircAttacker() {Center = CircSpawn}); //Skapar en ny Rektangel i rectangles listan
-        LastTimeSpawned = (float)Raylib.GetTime();   
+        EnemyCircles.Add(new CircAttacker() {Position = EnemyCircSpawn}); //Skapar en ny Rektangel i rectangles listan
+        EnemyLastTimeSpawned = (float)Raylib.GetTime();   
+    }
+
+    if(Raylib.IsKeyPressed(KeyboardKey.Two) && ((float)Raylib.GetTime() - PlrLastTimeSpawned >= CooldownTime))
+    {
+        PlrTriangles.Add(new TriAttacker() {Position = new Vector2(PlrSpawnX, SpawnY - 40)}); //Skapar en ny Rektangel i rectangles listan
+        PlrLastTimeSpawned = (float)Raylib.GetTime();   
+    }
+
+    if(Raylib.IsKeyPressed(KeyboardKey.Nine) && ((float)Raylib.GetTime() - EnemyLastTimeSpawned >= CooldownTime))
+    {
+        EnemyTriangles.Add(new TriAttacker() {Position = new Vector2(EnemySpawnX, SpawnY - 40)}); //Skapar en ny Rektangel i rectangles listan
+        EnemyLastTimeSpawned = (float)Raylib.GetTime();   
+    }
+
+    if(Raylib.IsKeyPressed(KeyboardKey.Three) && ((float)Raylib.GetTime() - PlrLastTimeSpawned >= CooldownTime))
+    {
+        PlrRectangles.Add(new RectAttacker() {Rect = new Rectangle(PlrSpawnX - 40, SpawnY - 40, 80, 80)}); //Skapar en ny Rektangel i rectangles listan
+        PlrLastTimeSpawned = (float)Raylib.GetTime();   
+    }
+
+    if(Raylib.IsKeyPressed(KeyboardKey.Zero) && ((float)Raylib.GetTime() - EnemyLastTimeSpawned >= CooldownTime))
+    {
+        EnemyRectangles.Add(new RectAttacker() {Rect = new Rectangle(EnemySpawnX - 40, SpawnY - 40, 80, 80)}); //Skapar en ny Rektangel i rectangles listan
+        EnemyLastTimeSpawned = (float)Raylib.GetTime();   
     }
 }
 
-void Draw() //Ritar alla anfallare som har skapats
+void Draw() //Ritar alla spelarens och motståndarens anfallare som har skapats
 {
-    for (int i = 0; i < rectangles.Count; i++)
+    for (int i = 0; i < PlrCircles.Count; i++)
     {
-        RectAttacker rect = rectangles[i];
+        CircAttacker center = PlrCircles[i];
+        Raylib.DrawCircleV(center.Position, center.radius, Color.Black);
+        Raylib.DrawCircleV(center.Position, center.radius - 4, Color.Blue);
+    }
+
+    for (int i = 0; i < EnemyCircles.Count; i++)
+    {
+        CircAttacker center = EnemyCircles[i];
+        Raylib.DrawCircleV(center.Position, center.radius, Color.Black);
+        Raylib.DrawCircleV(center.Position, center.radius - 4, Color.Blue);
+    }
+     
+    for (int i = 0; i < PlrTriangles.Count; i++)
+    {
+        TriAttacker tri = PlrTriangles[i];
+        Vector2 p1 = tri.Position;
+        Vector2 p2 = new Vector2(tri.Position.X - tri.Size / 2, tri.Position.Y + tri.Size);
+        Vector2 p3 = new Vector2(tri.Position.X + tri.Size / 2, tri.Position.Y + tri.Size);
+
+        Raylib.DrawTriangle(p1, p2, p3, Color.Black);
+        Raylib.DrawTriangle(p1 + new Vector2(0,8), p2 + new Vector2(6,-4), p3 - new Vector2(6,4), Color.Green);
+    }
+
+    for (int i = 0; i < EnemyTriangles.Count; i++)
+    {
+        TriAttacker tri = EnemyTriangles[i];
+        Vector2 p1 = tri.Position;
+        Vector2 p2 = new Vector2(tri.Position.X - tri.Size / 2, tri.Position.Y + tri.Size);
+        Vector2 p3 = new Vector2(tri.Position.X + tri.Size / 2, tri.Position.Y + tri.Size);
+
+        Raylib.DrawTriangle(p1, p2, p3, Color.Black);
+        Raylib.DrawTriangle(p1 + new Vector2(0,8), p2 + new Vector2(6,-4), p3 - new Vector2(6,4), Color.Green);
+    }
+
+     for (int i = 0; i < PlrRectangles.Count; i++)
+    {
+        RectAttacker rect = PlrRectangles[i];
         Raylib.DrawRectangleRec(rect.Rect, Color.Red);
         Raylib.DrawRectangleLinesEx(rect.Rect, 4, Color.Black);
     }
 
-    for (int i = 0; i < circles.Count; i++)
+    for (int i = 0; i < EnemyRectangles.Count; i++)
     {
-        CircAttacker center = circles[i];
-        Raylib.DrawCircleV(center.Center, center.radius + 4, Color.Black);
-        Raylib.DrawCircleV(center.Center, center.radius, Color.Blue);
-
+        RectAttacker rect = EnemyRectangles[i];
+        Raylib.DrawRectangleRec(rect.Rect, Color.Red);
+        Raylib.DrawRectangleLinesEx(rect.Rect, 4, Color.Black);
     }
 }
 
-void UpdateMovement()
+void UpdateMovement() //Method som flyttar anfallarna mot motståndarens bas
 {
-    for (int i = 0; i < rectangles.Count; i++)
+    for (int i = 0; i < PlrCircles.Count; i++)
     {
-        RectAttacker rect = rectangles[i];
-        rect.Rect.X += rect.Speed;
+        CircAttacker center = PlrCircles[i];
+        if(center.Position.X + center.radius < 1500)
+        {
+            center.Position.X += center.Speed;
+        }else
+        {
+            center.Speed = 0;
+        }
     }
 
-     for (int i = 0; i < circles.Count; i++)
+    for (int i = 0; i < EnemyCircles.Count; i++)
     {
-        CircAttacker center = circles[i];
-        center.Center.X += center.Speed;
+        CircAttacker center = EnemyCircles[i];
+         if(center.Position.X - center.radius > 0)
+        {
+            center.Position.X -= center.Speed;
+        }else
+        {
+            center.Speed = 0;
+        }
+    }
+
+    for (int i = 0; i < PlrTriangles.Count; i++)
+    {
+        TriAttacker tri = PlrTriangles[i];
+        if(tri.Position.X + tri.Size/2 < 1500)
+        {
+        tri.Position.X += tri.Speed;
+        } else
+        {
+            tri.Speed = 0;
+        }
+    }
+
+    for (int i = 0; i < EnemyTriangles.Count; i++)
+    {
+        TriAttacker tri = EnemyTriangles[i];
+         if(tri.Position.X - tri.Size/2> 0)
+        {
+        tri.Position.X -= tri.Speed;
+        } else
+        {
+            tri.Speed = 0;
+        }
+    }
+
+    for (int i = 0; i < PlrRectangles.Count; i++)
+    {
+        RectAttacker rect = PlrRectangles[i];
+        if(rect.Rect.X + rect.Rect.Width < 1500)
+        {
+        rect.Rect.X += rect.Speed;
+        } else
+        {
+            rect.Speed = 0;
+        }
+
+    }
+
+    for (int i = 0; i < EnemyRectangles.Count; i++)
+    {
+        RectAttacker rect = EnemyRectangles[i];
+        if(rect.Rect.X> 0)
+        {
+        rect.Rect.X -= rect.Speed;  
+        } else
+        {
+            rect.Speed = 0;
+        }
     }
 }
+
+void Fighting()
+{}
 
 Raylib.InitWindow(1500,800, "Attack Of The Blobs!");
 
@@ -79,5 +204,6 @@ while(Raylib.WindowShouldClose() == false)
     SpawnButton();
     Draw();
     UpdateMovement();
+    Fighting();
     Raylib.EndDrawing();
 } 
